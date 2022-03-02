f928886c75d2c6cc3ff2b0303f1e24001aae31d5
Bvaishnavi123 <97448974+Bvaishnavi123@users.noreply.github.com>
2022-03-02 20:29:46 +0530

    Assignment Done

---
+++ question2/division.js
@@ -0,0 +1,6 @@
+function division(a,b)
+{
+    return Math.floor(a/b)
+}
+
+module.exports = division;
---
+++ question2/main.js
@@ -0,0 +1,11 @@
+let addition = require("./add")
+console.log(addition(2,3))
+
+let substract = require("./substract")
+console.log(substract(2,3))
+
+let division = require("./division")
+console.log(division(10,5))
+
+let multiplication = require("./multiply")
+console.log(multiplication(10,5))
